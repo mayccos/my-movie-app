1bf4abdad62a1ba4fff1156d1300a01244ed2f09
mayccos <mothos@hotmail.fr>
2024-09-01 00:02:41 +0200

    refresh home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Genres from "@/components/Genres/Genres";
 import styles from "./page.module.css";
 import Popular from "@/components/popular/Popular";
 
+export const revalidate = 86400;
+
 export default function Home() {
   return (
     <div className={styles.main}>
